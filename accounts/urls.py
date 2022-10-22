dbb117be4045f0efb2b1bf558d8735b316fe8a4e
mavhungutrezzy <mavhungutrezzy@gmail.com>
2022-10-22 17:12:05 +0200

    feat - add authentication functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.authentication, name='authentication'),
+    path('accounts/signin/', views.signin, name='signin'),
+    path('accounts/logout/', views.logout, name='logout'),
+    path('accounts/register/', views.register, name='register'),
 ]
