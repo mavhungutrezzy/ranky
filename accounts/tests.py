4afb9b6746fb76b12a49c2679f64e1b9184caa02
mavhungutrezzy <mavhungutrezzy@gmail.com>
2022-10-22 17:18:08 +0200

    test - add test for signin and register templates

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from http import HTTPStatus
 
-# Create your tests here.
+
+class RegisterTestCase(TestCase):
+    
+    def setUp(self):
+        url = reverse('register')
+        self.response = self.client.get(url)
+    
+    def test_register_template(self):
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(self.response, 'register.html')
+        self.assertContains(self.response, 'Register')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+        
+
+class SignInTestCase(TestCase):
+    
+    def setUp(self):
+        url = reverse('signin')
+        self.response = self.client.get(url)
+    
+    def test_signin_template(self):
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(self.response, 'signin.html')
+        self.assertContains(self.response, 'Sign In')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
