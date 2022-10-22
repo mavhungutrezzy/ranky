2136c02958909f042fbd17816ab7b49980ea67a9
mavhungutrezzy <mavhungutrezzy@gmail.com>
2022-10-22 20:56:38 +0200

    test - add test for detail page template🧪

--- home/tests.py
+++ home/tests.py
@@ -4,14 +4,26 @@ from http import HTTPStatus
 
 
 class HomeTestCase(TestCase):
-    
     def setUp(self):
-        url = reverse('home')
+        url = reverse("home")
         self.response = self.client.get(url)
-    
+
     def test_home_template(self):
         self.assertEqual(self.response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(self.response, 'home.html')
-        self.assertContains(self.response, 'Restaurants')
-        self.assertContains(self.response, 'Mavhungu Adivhaho')
-        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+        self.assertTemplateUsed(self.response, "home.html")
+        self.assertContains(self.response, "Restaurants")
+        self.assertContains(self.response, "Mavhungu Adivhaho")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+
+
+class DetailTestCase(TestCase):
+    def setUp(self):
+        url = reverse("detail")
+        self.response = self.client.get(url)
+
+    def test_detail_template(self):
+        self.assertEqual(self.response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(self.response, "detail.html")
+        self.assertContains(self.response, "Ramen Grannies")
+        self.assertContains(self.response, "More places around here")
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
